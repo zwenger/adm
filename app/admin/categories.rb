568dbdb4d3d4439f2ddc17132d39bfd150a18443
zwenger <ritortoalvaro93@gmail.com>
2018-11-22 21:52:16 -0300

    modifique el display_name de categoria para que muestre la descripcion

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -12,4 +12,5 @@ ActiveAdmin.register Category do
 #   permitted
 # end
 
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
     has_many :categorizations
     has_many :providers, through: :categorizations
     has_many :equipments
+    
+    def display_name
+        return self.description
+    end
 end
